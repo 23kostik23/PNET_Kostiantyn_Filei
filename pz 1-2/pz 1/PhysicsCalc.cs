831dab66427811e67720024c46947ccfa1b999d5
23kostik23 <kk648604@gmail.com>
2026-06-27 00:31:57 +0200

    pz 1

---
+++ pz 1-2/pz 1/PhysicsCalc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Utils
+{
+    public class PhysicsCalc
+    {
+        public static double CalculateDistance(double velocity, double angleDegrees)
+        {
+            double g = 9.81;
+            double angleRadians = angleDegrees * Math.PI / 180.0;
+
+            // Формула: L = (v^2 * sin(2*alpha)) / g
+            double distance = (Math.Pow(velocity, 2) * Math.Sin(2 * angleRadians)) / g;
+
+            return Math.Round(distance, 2);
+        }
+    }
+}
---
+++ pz 1-2/pz 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Utils;
+
+class Program
+{
+    static void Main()
+    {
+        double v = 20.0;
+        double angle = 45.0;
+
+        double distance = PhysicsCalc.CalculateDistance(v, angle);
+
+        Console.WriteLine($"Початкова швидкість: {v} м/с");
+        Console.WriteLine($"Кут кидка: {angle} градусів");
+        Console.WriteLine($"Дальність польоту: {distance} м");
+        Console.ReadLine();
+    }
+}
